5158400979f4f90eb879ef961909d54724fa937e
Asiya Shaik <asiyacse504@gmail.com>
2024-11-24 11:36:36 +0530

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000" });
+const API = axios.create({ baseURL: "https://memories-project-backend-rg1w.onrender.com" });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("profile")) {
