1b88de96184fcb9873ed8fd61e2f065347e5bd2f
Asiya Shaik <asiyacse504@gmail.com>
2024-11-25 12:04:52 +0530

    changes in auth

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const auth = async (req, res, next) => {
-  const JWT_SECRET = process.env.JWT_SECRET_KEY || "test";
+  const JWT_SECRET = process.env.JWT_SECRET_KEY;
 
   try {
     const authHeader = req.headers.authorization;
